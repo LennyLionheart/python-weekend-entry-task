36125f3739e27f1f8e16582d581cb53a339a6dc2
LennyLionheart <65294897+LennyLionheart@users.noreply.github.com>
2022-02-15 17:29:09 +0100

    fixed command-line options

--- solution.py
+++ solution.py
@@ -1,10 +1,29 @@
 import csv
 import datetime
+import sys
+import argparse
+
+#evaluate command-line inputs
+parser = argparse.ArgumentParser(description = 'Flight comparing program.')
+parser.add_argument('flightdata', metavar = 'F', type = str, help = 'filepath for the csv containing the flight data' )
+parser.add_argument('origin', metavar = 'O', type = str, help = 'Origin airport code')
+parser.add_argument('destination', metavar = 'D', type = str, help = 'Destination airport code')
+parser.add_argument('--bags', type = int, default = 0, help = 'Number of requested bags')
+parser.add_argument('--return', dest = 'returntrip', action = 'store_true', help = 'Is it a return flight?')
+
+args = parser.parse_args()
+
+#inputs
+flightdata = args.flightdata
+origin = args.origin
+destination = args.destination
+bagcount = args.bags
+returntrip = args.returntrip
 
 
 # open file and put the flights into tuples
 try:
-    with open('example/example0.csv', 'r') as read_obj:
+    with open(str(flightdata), 'r') as read_obj:
 
         csv_reader = csv.reader(read_obj)
 
@@ -26,11 +45,11 @@ try:
                 if x == 5 or x == 6:
                     formatted_list_of_flights[i][x] = float(list_of_flights[i][x])
 
-                # allowed bagscount
+                # allowed bagcount
                 if  x == 7:
                     formatted_list_of_flights[i][x] = int(list_of_flights[i][x])
 
-        # turning data into tuples...  does this make any sense with repeated bigger company size calculations?
+        # turning data into tuples...  does this make any sense with repeated big company size calculations?
         for i in range(len(formatted_list_of_flights)):
             formatted_list_of_flights[i] = tuple(formatted_list_of_flights[i])
 
@@ -39,3 +58,12 @@ try:
 
 finally:
     read_obj.close()
+
+
+print(flightdata)
+print(origin)
+print(destination)
+print(bagcount)
+print(returntrip)
+
+print(tuple_of_flights)
