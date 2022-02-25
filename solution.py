22f275e8d101ac14b8071e2b783535b4786917dd
LennyLionheart <65294897+LennyLionheart@users.noreply.github.com>
2022-02-25 06:57:15 +0100

    added functions and bug fixes

--- solution.py
+++ solution.py
@@ -4,7 +4,7 @@ import sys
 import argparse
 import json
 
-#evaluate command-line inputs
+# evaluate command-line inputs
 parser = argparse.ArgumentParser(description = 'Flight comparing program.')
 parser.add_argument('flightdata', metavar = 'F', type = str, help = 'filepath for the csv containing the flight data' )
 parser.add_argument('origin', metavar = 'O', type = str, help = 'Origin airport code')
@@ -14,7 +14,7 @@ parser.add_argument('--bags', type = int, default = 0, help = 'Number of request
 
 args = parser.parse_args()
 
-#inputs
+# inputs
 flightdata = args.flightdata
 origin = args.origin
 destination = args.destination
@@ -23,34 +23,67 @@ bags_count = args.bags
 
 
 # open file and put the flight into a tuple of dictionaries
-try:
-    with open(str(flightdata), 'r') as read_obj:
 
-        list_of_flights = []
+with open(str(flightdata), 'r') as read_obj:
 
-        csv_reader = csv.DictReader(read_obj)
-        for line in csv_reader:
-            list_of_flights.append(line)
+    list_of_flights = []
 
-        for dict in list_of_flights:
-            dict['departure'] = datetime.datetime.strptime(dict['departure'],'%Y-%m-%dT%H:%M:%S')
-            dict['arrival'] = datetime.datetime.strptime(dict['arrival'],'%Y-%m-%dT%H:%M:%S')
-            dict['base_price'] = float(dict['base_price'])
-            dict['bag_price'] = float(dict['bag_price'])
-            dict['bags_allowed'] = int(dict['bags_allowed'])
+    csv_reader = csv.DictReader(read_obj)
+    for line in csv_reader:
+        list_of_flights.append(line)
 
-        # dropping low baggage flights if necessecery
-        if bags_count == 2:
+    for flight in list_of_flights:
+        flight['departure'] = datetime.datetime.strptime(flight['departure'],'%Y-%m-%dT%H:%M:%S')
+        flight['arrival'] = datetime.datetime.strptime(flight['arrival'],'%Y-%m-%dT%H:%M:%S')
+        flight['base_price'] = float(flight['base_price'])
+        flight['bag_price'] = float(flight['bag_price'])
+        flight['bags_allowed'] = int(flight['bags_allowed'])
 
-            two_bag_list_of_flights = [dict for dict in list_of_flights if dict['bags_allowed'] == 2]
-            list_of_flights = two_bag_list_of_flights
+    # dropping low baggage flights if necessary
+    if bags_count == 2:
+
+        two_bag_list_of_flights = [flight for flight in list_of_flights if flight['bags_allowed'] == 2]
+        list_of_flights = two_bag_list_of_flights
 
 
 
-        tuple_of_flights = tuple(list_of_flights)
+tuple_of_flights = tuple(list_of_flights)
 
-finally:
-    read_obj.close()
+def flightsearchloop(*flightcheck): # function for assembling the right flight solutions
+
+    global flight_solutions
+    global destination
+
+    # filtering for no repeating airports
+    repeating_airport_condition = "False"
+    origin_list = []
+
+    for x in range(len(flightcheck)):
+        origin_list.append(flightcheck[x]['origin'])
+
+    if len(origin_list) == len(set(origin_list)):
+            repeating_airport_condition = "True"
+
+
+    if (flightcheck[-2]['destination'] == flightcheck[-1]['origin'] and
+        repeating_airport_condition == "True" and
+        flightcheck[-1]['departure'] - flightcheck[-2]['arrival'] > datetime.timedelta(hours = 1) and # filtering the layover times
+        flightcheck[-1]['departure'] - flightcheck[-2]['arrival'] < datetime.timedelta(hours = 6)): # filtering the layover times
+
+              if flightcheck[-1]['destination'] == destination:
+
+                  to_append = []
+
+                  for i in flightcheck:
+                      to_append.append(i)
+
+                  flight_solutions.append(to_append)
+                  return True
+              else:
+                  return False
+
+    else:
+        return True
 
 flight_solutions = []
 # one flight permutations
@@ -60,43 +93,40 @@ for flight1 in tuple_of_flights:
 
         if flight1['destination'] == destination:
             flight_solutions.append([flight1])
-
         else:
             # two flight permutations of the not direct flights
             for flight2 in tuple_of_flights:
 
-                if (flight1['destination'] == flight2['origin'] and
-                    flight2['departure'] - flight1['arrival'] > datetime.timedelta(hours = 1) and # filtering the layover times
-                    flight2['departure'] - flight1['arrival'] < datetime.timedelta(hours = 6)): # filtering the layover times
+                if flightsearchloop(flight1, flight2):
+                    pass
+                else:
+                    # three flight permutations of the not direct two flight permutations
+                    for flight3 in tuple_of_flights:
 
-                        if flight2['destination'] == destination:
-                            flight_solutions.append([flight1, flight2])
-
+                        if flightsearchloop(flight1, flight2, flight3):
+                            pass
                         else:
-                            # three flight permutations of the not direct two flight permutations
-                            for flight3 in tuple_of_flights:
+                            # four flight permutations of the not direct three flight permutations
+                            for flight4 in tuple_of_flights:
 
-                                if (flight2['destination'] == flight3['origin'] and
-                                    flight3['origin'] != origin and # no airport repetition
-                                    flight3['departure'] - flight2['arrival'] > datetime.timedelta(hours = 1) and # filtering the layover times
-                                    flight3['departure'] - flight2['arrival'] < datetime.timedelta(hours = 6)): # filtering the layover times
+                                if flightsearchloop(flight1, flight2, flight3, flight4):
+                                    pass
+                                else:
+                                    # five flight permutations of the not direct three flight permutations
+                                    for flight5 in tuple_of_flights:
 
-                                    if flight3['destination'] == destination:
-                                        flight_solutions.append([flight1, flight2, flight3])
+                                        if flightsearchloop(flight1, flight2, flight3, flight4, flight5):
+                                            pass
+                                        else:
+                                            # six flight permutations of the not direct three flight permutations
+                                            for flight6 in tuple_of_flights:
 
-                                    else:
-                                        # four flight permutations of the not direct three flight permutations
-                                        for flight4 in tuple_of_flights:
-
-                                            if (flight3['destination'] == flight4['origin'] and
-                                                flight3['origin'] != origin and # no airport repetition
-                                                flight4['origin'] != origin and # no airport repetition
-                                                flight4['origin'] != flight2['origin'] and # no airport repetition
-                                                flight4['departure'] - flight3['arrival'] > datetime.timedelta(hours = 1) and # filtering the layover times
-                                                flight4['departure'] - flight3['arrival'] < datetime.timedelta(hours = 6)): # filtering the layover times
-
-                                                if flight4['destination'] == destination:
-                                                    flight_solutions.append([flight1, flight2, flight3, flight4])
+                                                if flightsearchloop(flight1, flight2, flight3, flight4, flight5, flight6):
+                                                    pass
+                                                else:
+                                                    # seven flight permutations of the not direct three flight permutations
+                                                    for flight7 in tuple_of_flights:
+                                                        flightsearchloop(flight1, flight2, flight3, flight4, flight5, flight6, flight7)
 
 
 
@@ -113,7 +143,7 @@ for flight in flight_solutions:
 
         total_price += flight[i]['base_price'] + bags_count * flight[i]['bag_price']
 
-        if flight[i]['bags_allowed'] == 1: #filtering for baggage limits
+        if flight[i]['bags_allowed'] == 1: # filtering for baggage limits
             bags_allowed = 1
 
     jsonlist.append({
@@ -123,7 +153,7 @@ for flight in flight_solutions:
                         'destination' : destination,
                         'origin' : origin,
                         'total_price' : total_price,
-                        'travel_time' : travel_time
+                        'travel_time' : travel_time,
                     })
 
 # sorting based on price
@@ -132,3 +162,4 @@ jsonlist.sort(key = lambda x: x.get('total_price'))
 json_string = json.dumps(jsonlist, indent = 4, default = str)
 
 print(json_string)
+print(len(flight_solutions))
